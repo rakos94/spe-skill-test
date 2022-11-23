1f188e644463878bd1886a3a42895cd60cd32ca4
kamal <rakos94@yahoo.com>
2022-11-23 17:01:54 +0700

    add case 111

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
-import(
+import (
 	"log"
-	"strconv"
 	"math"
+	"strconv"
 )
 
-func main(){
+func main() {
 	log.Println(narcissistic(153))
+	log.Println(narcissistic(111))
 }
 
 func narcissistic(value int) bool {
@@ -15,14 +16,14 @@ func narcissistic(value int) bool {
 	slice := strconv.Itoa(value)
 	l := len(slice)
 	sum := 0
-	
+
 	// log.Println(value / 10)
 	for value%10 > 0 {
-		lastDigit := value%10
-		sum += int(math.Pow(float64(lastDigit),float64(l)))
+		lastDigit := value % 10
+		sum += int(math.Pow(float64(lastDigit), float64(l)))
 		value = value / 10
 	}
-	
+
 	if sum == realValue {
 		return true
 	}
